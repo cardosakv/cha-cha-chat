5091b0e7bdff9b6df53f6e82e6dc3081a068072b
cardosakv <kvcards26@gmail.com>
2025-05-25 23:26:59 +0800

    Add nuxt fonts

--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -3,7 +3,5 @@ import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss',
-  ]
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/fonts']
 })
