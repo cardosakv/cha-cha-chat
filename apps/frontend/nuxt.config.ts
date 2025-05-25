321704406d076a283fd68a2e8f3b5f22118a2c50
cardosakv <kvcards26@gmail.com>
2025-05-25 22:07:46 +0800

    Fix tailwind config

--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -3,12 +3,7 @@ import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
-  tailwindcss: {
-    extend: {
-      colors: {
-
-      },
-    },
-  },
+  modules: [
+    '@nuxtjs/tailwindcss',
+  ]
 })
