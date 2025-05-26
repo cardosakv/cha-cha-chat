df783dd1b38947768762448ced13812e5459c3c7
cardosakv <kvcards26@gmail.com>
2025-05-26 15:04:38 +0800

    Add @vueuse/core

--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -3,6 +3,6 @@ import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/eslint", "nuxt-mdi"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/eslint", "nuxt-mdi", "@vueuse/nuxt"],
   css: ["~/assets/css/main.css"]
 });
