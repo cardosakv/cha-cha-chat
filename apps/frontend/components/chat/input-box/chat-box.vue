2d54f1be7d13bd78b8d87a5d054698063d9eee69
cardosakv <kvcards26@gmail.com>
2025-05-30 15:18:39 +0800

    Allow sending of image files

--- apps/frontend/components/chat/input-box/chat-box.vue
+++ apps/frontend/components/chat/input-box/chat-box.vue
@@ -1,24 +1,45 @@
 <script lang="ts" setup>
 import type { EmojiExt } from "vue3-emoji-picker";
 
-const message = defineModel<string>();
+const text = defineModel<string>();
 const emit = defineEmits<{
-  (e: "send"): any;
-  (e: "select-file"): any;
+  (e: "send-text", text: string): any;
+  (e: "send-file", imageBase64: string): any;
 }>();
 
+const { open, onChange } = useFileDialog({ accept: "image/*", multiple: false, reset: true });
+
+onChange((files) => {
+  const file = files?.item(0);
+  if (!file) return;
+
+  const reader = new FileReader();
+  reader.readAsDataURL(file);
+
+  reader.onload = () => emit("send-file", reader.result as string);
+});
+
 function addEmoji(emoji: EmojiExt) {
-  if (!message.value) {
-    message.value = "";
+  if (!text.value) {
+    text.value = "";
   }
 
-  message.value = message.value + emoji.i;
+  text.value = text.value + emoji.i;
+}
+
+function sendText() {
+  if (!text.value) {
+    text.value = "";
+  }
+
+  emit("send-text", text.value);
+  text.value = "";
 }
 </script>
 
 <template>
-  <ChatBoxIconButton icon="mdiFile" @click="emit('select-file')" />
+  <ChatBoxIconButton icon="mdiFile" @click="open" />
   <ChatBoxEmojiPicker @select="addEmoji" />
-  <ChatBoxInput v-model="message" @keydown-enter="emit('send')" />
-  <ChatBoxIconButton icon="mdiSend" @click="emit('send')" />
+  <ChatBoxInput v-model="text" @keydown-enter="sendText" />
+  <ChatBoxIconButton icon="mdiSend" @click="sendText" />
 </template>
