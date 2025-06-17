af91b544ffe1240bba6c7602325a1ef43de6b9d3
cardosakv <kvcards26@gmail.com>
2025-06-17 21:49:18 +0800

    Add vitest in frontend

--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/eslint", "nuxt-mdi", "@vueuse/nuxt"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/eslint", "nuxt-mdi", "@vueuse/nuxt", "@nuxt/test-utils/module"],
   css: ["~/assets/css/main.css"],
   components: [
     { path: "~/components", pathPrefix: false },
