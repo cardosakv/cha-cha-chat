cff7f466205eea15a330543aa3711e26dff6f79f
cardosakv <kvcards26@gmail.com>
2025-08-23 03:49:32 +0800

    Remove fe-start.sh script for frontend setup

--- fe-start.sh
+++
@@ -1,9 +0,0 @@
-# Install local pnpm
-npm install -D pnpm
-
-# Install & build
-npx pnpm install --filter cha-cha-chat-fe --shamefully-hoist
-npx pnpm build --filter cha-cha-chat-fe
-
-# Start server
-PORT=8080 node apps/frontend/.output/server/index.mjs
