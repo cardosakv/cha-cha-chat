5a4e7737a9bbda3844687b16c0f4881fc2e2fe75
cardosakv <kvcards26@gmail.com>
2025-08-23 02:58:51 +0800

    Remove cleanup of node_modules from fe-start.sh

--- fe-start.sh
+++ fe-start.sh
@@ -1,10 +1,6 @@
 # Install local pnpm
 npm install -D pnpm
 
-# Clean node_modules
-rm -rf node_modules
-pnpm store prune
-
 # Install & build
 npx pnpm install --filter cha-cha-chat-fe --shamefully-hoist
 npx pnpm build --filter cha-cha-chat-fe
